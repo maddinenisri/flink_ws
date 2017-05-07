2a9337538c7603cb5546bf6140ad553170f966c5
Srini Maddineni <smaddineni@mdstechinc.com>
2017-05-07 17:31:03 -0400

    Used SQL for alternative

--- sample-wc/src/main/java/com/mdstech/sample/csv/CsvSqlParser.java
+++ sample-wc/src/main/java/com/mdstech/sample/csv/CsvSqlParser.java
@@ -11,10 +11,12 @@ import org.apache.flink.table.api.Table;
 import org.apache.flink.table.api.TableEnvironment;
 import org.apache.flink.table.api.Types;
 import org.apache.flink.table.api.java.BatchTableEnvironment;
+import org.apache.flink.table.expressions.Expression;
 import org.apache.flink.table.sources.CsvTableSource;
 import org.apache.flink.types.IntValue;
 import org.apache.flink.types.Row;
 import org.apache.flink.types.StringValue;
+import scala.collection.Seq;
 
 import java.sql.SQLType;
 import java.time.LocalDateTime;
@@ -28,30 +30,48 @@ public class CsvSqlParser {
 
         final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
         final BatchTableEnvironment tableEnv = TableEnvironment.getTableEnvironment( env );
-        CsvTableSource userTableSource = new CsvTableSource("datafiles/users1M.csv", "id,fname,lname,age,company".split(","),
-                new TypeInformation[]{ Types.INT(), Types.STRING(), Types.STRING(), Types.INT(), Types.STRING() },
+
+        CsvTableSource userTableSource = new CsvTableSource("datafiles/sample1M.csv", "key,id,fname,lname,age,company".split(","),
+                new TypeInformation[]{ Types.INT(), Types.INT(), Types.STRING(), Types.STRING(), Types.INT(), Types.STRING() },
                 "|", CsvInputFormat.DEFAULT_LINE_DELIMITER, '"',
                 true, null, true);
         tableEnv.registerTableSource("users", userTableSource);
         Table userTable = tableEnv.scan("users");
-        Table subSetTable = userTable.select("id, fname, lname");
-        DataSet<Row> result = tableEnv.toDataSet(subSetTable, Row.class);
-        //result.print();
+        Table userTable2 = tableEnv.scan("users");
 
-//        Table subSetTable2 = userTable.select("id, fname, lname,  age, company").where("id = 0");
-//        DataSet<UserDetail> result2 = tableEnv.toDataSet(subSetTable2, UserDetail.class);
-//        result2.print();
+
+        //userTable.where("key = '10'").select("id, fname, lname, age, company");
+        Table subSetTable2 =
+        tableEnv.sql("SELECT id, fname, lname, age, company FROM users WHERE key = 20");
+        DataSet<Row> result2 = tableEnv.toDataSet(subSetTable2, Row.class);
+        JDBCOutputFormat.JDBCOutputFormatBuilder outputBuilder2 =
+                JDBCOutputFormat.buildJDBCOutputFormat()
+                        .setDrivername("org.postgresql.Driver")
+                        .setDBUrl("jdbc:postgresql://localhost:5432/postgres")
+                        .setUsername("postgres")
+                        .setPassword("postgres")
+                        .setQuery("insert into member_det (id,fname,lname,age,company) values (?,?,?,?,?)")
+                        .setSqlTypes(new int[] { java.sql.Types.INTEGER, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR, java.sql.Types.INTEGER, java.sql.Types.VARCHAR });
+
+        result2.output(outputBuilder2.finish());
+
+//        env.execute();
+
+        Table subSetTable = tableEnv.sql("SELECT id, fname, lname FROM users WHERE key = 10");
+                //userTable2.where("key = 10").select("id, fname, lname");
+        DataSet<Row> result = tableEnv.toDataSet(subSetTable, Row.class);
 
         JDBCOutputFormat.JDBCOutputFormatBuilder outputBuilder =
-            JDBCOutputFormat.buildJDBCOutputFormat()
-                .setDrivername("org.postgresql.Driver")
-                .setDBUrl("jdbc:postgresql://localhost:5432/postgres")
-                .setUsername("postgres")
-                .setPassword("postgres")
-                .setQuery("insert into members (id,fname, lname) values (?,?,?)")
-                .setSqlTypes(new int[] { java.sql.Types.INTEGER, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR });
+                JDBCOutputFormat.buildJDBCOutputFormat()
+                        .setDrivername("org.postgresql.Driver")
+                        .setDBUrl("jdbc:postgresql://localhost:5432/postgres")
+                        .setUsername("postgres")
+                        .setPassword("postgres")
+                        .setQuery("insert into user_map (id,first_name,last_name) values (?,?,?)")
+                        .setSqlTypes(new int[] { java.sql.Types.INTEGER, java.sql.Types.VARCHAR, java.sql.Types.VARCHAR });
 
         result.output(outputBuilder.finish());
+
         env.execute();
 
         LocalDateTime endDateTime = LocalDateTime.now();
